97b22bbb4a77302de26c382a8d4783d1c785ca0a
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-10-23 06:49:33 +0000

    Fibonacci

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
 import java.util.*;
-
-class Main {
-    public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int a = sc.nextInt();
-      String b = sc.nextLine();
-      System.out.println(a+ b);
-     sc.close();   
-    }
-}
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     int i=0;
+     int a=0;
+     int b=1;
+     while(i<n){
+         System.out.println(a);
+         int c=a+b;
+         a=b;
+         b=c;
+         i++;
+     }
+   }
+  }
