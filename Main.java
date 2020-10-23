1a9f0cd8dab0b7e529329040cf5aba4ece6d1a7e
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-10-23 08:05:09 +0000

    print digits

--- Main.java
+++ Main.java
@@ -1,16 +1,27 @@
 import java.util.*;
-    
+import java.lang.Math;
     public class Main{
     
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       int num = sc.nextInt();
-      int reversed=0;
-      while(num != 0) {
-            int digit = num % 10;
-            reversed = reversed * 10 + digit;
-            num = num / 10;
-        }
+      int temp=num;
+      int count=0;
+      while(temp!=0){
+          temp/=10;
+          count++;
+      }
+      
+      int val = (int)Math.pow(10,(count-1));
+      while(val!=0){
+          
+          int q = num/val;
+          System.out.println(q);
+          
+          num%=val;
+          val/=10;
+          
+      }
+      
+     }
     }
-    }
-        
