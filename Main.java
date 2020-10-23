9b9b4a28e4998c0040e2f160a012659985ac6b59
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-10-23 07:26:51 +0000

    count no of digits

--- Main.java
+++ Main.java
@@ -1,23 +1,17 @@
 import java.util.*;
- import java.lang.Math;
+  
   public class Main{
   
   public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int low = sc.nextInt();
-      int high = sc.nextInt();
-      for(int i=low;i<=high;i++){
-          int count=0;
-          double sq = Math.sqrt((double)i);
-          for(int j=2;j<=sq;j++){
-              if(i%j==0){
-                  count++;
-                  break;
-              }
-          }
-          if(count==0){
-          System.out.println(i);
-          }
-      }
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int count=0;
+    while(n!=0){
+
+        n=n/10;
+        count++;
+        
+    }
+    System.out.println(count);
    }
   }
