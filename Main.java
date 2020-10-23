2f3359478b0c0795ad70b764d754b7f904fee0f7
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-10-23 07:39:32 +0000

    reverse a number

--- Main.java
+++ Main.java
@@ -1,17 +1,16 @@
 import java.util.*;
-  
-  public class Main{
-  
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int count=0;
-    while(n!=0){
-
-        n=n/10;
-        count++;
-        
+    
+    public class Main{
+    
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int num = sc.nextInt();
+      int reversed=0;
+      while(num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num = num / 10;
+        }
     }
-    System.out.println(count);
-   }
-  }
+    }
+        
