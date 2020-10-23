2be1963f7c11b3f400f9cb84f573be12ab3de2c8
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-10-23 06:23:53 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      String b = sc.nextLine();
+      System.out.println(a+ b);
+     sc.close();   
+    }
+}
