18ed0324c8dce62b142222d7930605fa341cf125
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-10-23 06:59:36 +0000

    Prime or No

--- Main.java
+++ Main.java
@@ -1,19 +1,26 @@
 import java.util.*;
-  
+ import java.lang.Math;
   public class Main{
   
   public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in);
-     int n = sc.nextInt();
-     int i=0;
-     int a=0;
-     int b=1;
-     while(i<n){
-         System.out.println(a);
-         int c=a+b;
-         a=b;
-         b=c;
-         i++;
-     }
+      Scanner sc = new Scanner(System.in);
+      int t = sc.nextInt();
+      for(int i=0;i<t;i++){
+          double n= sc.nextDouble();
+          int count=0;
+          double x = Math.sqrt(n);
+          for(int j=2;j<(int)x;j++){
+              if(n%j==0){
+                  count+=1;
+                  break;
+              }
+          }
+          if(count==1){
+              System.out.println("not prime");
+          }else{
+              System.out.println("prime");
+          }
+      }
+  
    }
   }
