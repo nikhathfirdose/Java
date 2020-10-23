aa82a5e6356a2cd5f26a6f21b32bc7f51a9c6865
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-10-23 07:08:06 +0000

    Print all primes between range

--- Main.java
+++ Main.java
@@ -4,23 +4,20 @@ import java.util.*;
   
   public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      int t = sc.nextInt();
-      for(int i=0;i<t;i++){
-          double n= sc.nextDouble();
+      int low = sc.nextInt();
+      int high = sc.nextInt();
+      for(int i=low;i<=high;i++){
           int count=0;
-          double x = Math.sqrt(n);
-          for(int j=2;j<(int)x;j++){
-              if(n%j==0){
-                  count+=1;
+          double sq = Math.sqrt((double)i);
+          for(int j=2;j<=sq;j++){
+              if(i%j==0){
+                  count++;
                   break;
               }
           }
-          if(count==1){
-              System.out.println("not prime");
-          }else{
-              System.out.println("prime");
+          if(count==0){
+          System.out.println(i);
           }
       }
-  
    }
   }
