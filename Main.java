1c48b0cad46e31bda143877fd7ecefa480e2d739
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-10-26 11:05:12 +0000

    any base to decimal

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
 import java.util.*;
 import java.lang.Math;
-    public class Main{
-    
-    public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in);
-     int n = sc.nextInt();
-     int prod = 1;
-     for(int i=1; i<=n; i++){
-       prod*=i;
-     }
-     System.out.println(prod);
-      
-     }
-    }
+  public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int b = scn.nextInt();
+      int d = getValueIndecimal(n, b);
+      System.out.println(d);
+   }
+  
+   public static int getValueIndecimal(int n, int b){
+      int ans =0;
+      int i=0;
+      while(n>0){
+         int rem = n%10;
+         ans+=rem*(int)Math.pow(b,i);
+         i++;
+         n/=10;
+      }
+      return ans;
+   }
+  }
