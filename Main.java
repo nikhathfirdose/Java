2dcea63c146ab8d705e9b8a9774165e60690da27
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-10-26 06:30:25 +0000

    factorial

--- Main.java
+++ Main.java
@@ -3,25 +3,13 @@ import java.lang.Math;
     public class Main{
     
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int num = sc.nextInt();
-      int temp=num;
-      int count=0;
-      while(temp!=0){
-          temp/=10;
-          count++;
-      }
-      
-      int val = (int)Math.pow(10,(count-1));
-      while(val!=0){
-          
-          int q = num/val;
-          System.out.println(q);
-          
-          num%=val;
-          val/=10;
-          
-      }
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     int prod = 1;
+     for(int i=1; i<=n; i++){
+       prod*=i;
+     }
+     System.out.println(prod);
       
      }
     }
