bc7f70965216a5bb8cda217b1c36bc56f8b04125
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-10-26 11:20:19 +0000

    any base to any base

--- Main.java
+++ Main.java
@@ -1,16 +1,29 @@
 import java.util.*;
-import java.lang.Math;
+import java.lang.Math; 
   public class Main{
   
   public static void main(String[] args) {
       Scanner scn = new Scanner(System.in);
       int n = scn.nextInt();
-      int b = scn.nextInt();
-      int dn = getValueInBase(n, b);
-      System.out.println(dn);
+      int sourceBase = scn.nextInt();
+     int  destBase= scn.nextInt();
+     int x = getValueIndecimal(n,sourceBase);
+     int y = getValueInBase(x,destBase);
+     System.out.println(y);
+   }  
+   
+   public static int getValueIndecimal(int n, int b){
+      int ans =0;
+      int i=0;
+      while(n>0){
+         int rem = n%10;
+         ans+=rem*(int)Math.pow(b,i);
+         i++;
+         n/=10;
+      }
+      return ans;
    }
-  
-   public static int getValueInBase(int n, int b){
+    public static int getValueInBase(int n, int b){
         int ans=0;
         int i=0;
         while(n>0){
