809435c938b53db8cfe2ea2f5efc67a22561008c
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-10-27 11:12:36 +0000

    check if num present in array or no

--- Main.java
+++ Main.java
@@ -4,24 +4,26 @@ import java.util.*;
 public class Main{
 
 public static void main(String[] args) throws Exception {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int[] arr = new int[n];
-    for(int i=0;i<arr.length;i++){
-        arr[i] = sc.nextInt();
+   Scanner sc = new Scanner(System.in);
+   int n = sc.nextInt();
+   int[] arr = new int[n];
+   for(int i=0; i<arr.length;i++){
+       arr[i] = sc.nextInt();
+   }
+   int d = sc.nextInt();
+   int count=0;
+   for(int j=0; j<arr.length;j++){
+       if(arr[j]==d){
+           System.out.println(j);
+           
+           break;
+       }
+       count++;
+   }
+//   System.out.println(count);
+    if(count==(arr.length)){
+        System.out.println(-1);
     }
-    int max=arr[0];
-    int min=arr[0];
-    for(int j=0;j<arr.length-1;j++){
-        
-        if(arr[j+1]>max){
-            max = arr[j+1];
-        }else if(arr[j+1]<min){
-            min = arr[j+1];
-        }
-    }
-    int result = max-min;
-    System.out.println(result);
  }
 
 }
