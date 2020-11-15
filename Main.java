f86e1350100afba58b6bd805f111a41d48899e77
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-11-15 05:06:50 +0000

    pallindrome

--- Main.java
+++ Main.java
@@ -1,31 +1,18 @@
-import java.io.*;
 import java.util.*;
 
 public class Main{
 
 public static void main(String[] args) {
   Scanner sc = new Scanner(System.in);
-  int a = sc.nextInt();
-  int count=0;
-  if(a%2==0 || a==1){
-    System.out.println("noya");
-    System.exit(0);
-
-  }
-  for(int i=3; i<Math.sqrt(a); i++){
-    if(a%i==0){
-      count++;
-      break;
-    }
-    
-  }
-  if(count==0){
+  String a = sc.next();
+  StringBuilder r = new StringBuilder();
+  r.append(a);
+  StringBuilder m = r.reverse();
+  if(m.toString().equals(a)){
     System.out.println("yes");
   }else{
     System.out.println("noya");
-    System.out.println(Math.sqrt(a));
   }
-
  }
 
 }
