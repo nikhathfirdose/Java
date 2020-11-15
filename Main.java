d2bb6037ed1810055238a1492d6a361cc2b14ef1
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-11-15 10:39:31 +0000

    2nd largest

--- Main.java
+++ Main.java
@@ -1,20 +1,25 @@
 import java.util.*;
 
+// import com.sun.tools.javac.util.ArrayUtils;
+
 public class Main{
 
 public static void main(String[] args) {
   Scanner sc = new Scanner(System.in);
-  String s = sc.nextLine();
-  int count=0;
-  for(int i=0;i<s.length();i++){
-    for(int j=i+1;j<s.length();j++){
-      if(s.charAt(i) == (s.charAt(j))){
-        count++;
-        System.out.println(s.charAt(i));
-        System.out.println(count);
-      }
-    }
+  int size = sc.nextInt();
+  int[] arr = new int[size];
+
+  for(int i=0; i<size; i++){
+    arr[i] = sc.nextInt();
   }
+
+  Arrays.sort(arr);
+
+
+ System.out.println(arr[size-2]);
+
+ 
+
  }
 
 }
