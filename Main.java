2556678b4cc39cbca07188af1e5301a866eec62c
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-11-15 04:56:59 +0000

    prime

--- Main.java
+++ Main.java
@@ -4,8 +4,28 @@ import java.util.*;
 public class Main{
 
 public static void main(String[] args) {
-  String[] arr = {"a", "k", "JJJJJ"};
-  System.out.println(arr.length);
+  Scanner sc = new Scanner(System.in);
+  int a = sc.nextInt();
+  int count=0;
+  if(a%2==0 || a==1){
+    System.out.println("noya");
+    System.exit(0);
+
+  }
+  for(int i=3; i<Math.sqrt(a); i++){
+    if(a%i==0){
+      count++;
+      break;
+    }
+    
+  }
+  if(count==0){
+    System.out.println("yes");
+  }else{
+    System.out.println("noya");
+    System.out.println(Math.sqrt(a));
+  }
+
  }
 
 }
