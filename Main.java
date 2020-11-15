d32ad61561c881dad0bcafd7528505e975746f75
Nikhath Firdose <nikhathfirdose3@gmail.com>
2020-11-15 12:46:12 +0000

    integer palindrome

--- Main.java
+++ Main.java
@@ -6,25 +6,15 @@ public class Main{
 
 public static void main(String[] args) {
   Scanner sc = new Scanner(System.in);
-  int num = sc.nextInt();
-  int temp=num;
-  int size = (int)Math.log10(num)+1;
-  int sum=0;
-  for(int i=0; i<size; i++){
-    int x = num%10;
-    int mul=1;
-    for(int j=0; j<size; j++){
-      mul*=x;
-    }
-    sum+=mul;
-    num/=10;
-
-  }
-  //  System.out.println(sum);153
-  if(sum == temp){
+  int a = sc.nextInt();
+  String num = Integer.toString(a);
+  StringBuilder sb = new StringBuilder();
+  sb.append(num);
+  StringBuilder rev =  sb.reverse();
+  if(rev.toString().equals(num)){
     System.out.println("yes");
   }else{
-     System.out.println("no");
+    System.out.println("no");
   }
   
 
